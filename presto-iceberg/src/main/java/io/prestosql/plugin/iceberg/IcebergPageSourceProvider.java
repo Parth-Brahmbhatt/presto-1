trinodb/trino

Make Files table distributed.

presto-iceberg/src/main/java/io/prestosql/plugin/iceberg/IcebergPageSourceProvider.java
@@ -61,6 +61,7 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hdfs.BlockMissingException;
 import org.apache.iceberg.FileFormat;
+import org.apache.iceberg.hadoop.HadoopFileIO;
 import org.apache.parquet.column.ColumnDescriptor;
 import org.apache.parquet.hadoop.metadata.BlockMetaData;
 import org.apache.parquet.hadoop.metadata.FileMetaData;
@@ -159,17 +160,26 @@ public class IcebergPageSourceProvider
                 .collect(toImmutableList());
 
         HdfsContext hdfsContext = new HdfsContext(session, table.getSchemaName(), table.getTableName());
-        ConnectorPageSource dataPageSource = createDataPageSource(
-                session,
-                hdfsContext,
-                new Path(split.getPath()),
-                split.getStart(),
-                split.getLength(),
-                split.getFileFormat(),
-                regularColumns,
-                table.getPredicate());
+        switch (table.getTableType()) {
+            case DATA:
+                ConnectorPageSource dataPageSource = createDataPageSource(
+                        session,
+                        hdfsContext,
+                        new Path(split.getPath()),
+                        split.getStart(),
+                        split.getLength(),
+                        split.getFileFormat(),
+                        regularColumns,
+                        table.getPredicate());
 
-        return new IcebergPageSource(icebergColumns, partitionKeys, dataPageSource, session.getTimeZoneKey());
+                return new IcebergPageSource(icebergColumns, partitionKeys, dataPageSource, session.getTimeZoneKey());
+            case FILES:
+                Path path = new Path(split.getPath());
+                HadoopFileIO fileIO = new HadoopFileIO(hdfsEnvironment.getConfiguration(hdfsContext, path));
+                return new FilesPageSource(fileIO, icebergColumns, split.getPath());
+            default:
+                throw new PrestoException(IcebergErrorCode.ICEBERG_UNKNOWN_TABLE_TYPE, "not supported" + table.getTableType());
+        }
     }
 
     private ConnectorPageSource createDataPageSource(

Parth-Brahmbhatt/presto-1

presto-iceberg/src/main/java/io/prestosql/plugin/iceberg/IcebergPageSourceProvider.java
@@ -52,6 +52,7 @@ import io.prestosql.spi.connector.ConnectorSession;
 import io.prestosql.spi.connector.ConnectorSplit;
 import io.prestosql.spi.connector.ConnectorTableHandle;
 import io.prestosql.spi.connector.ConnectorTransactionHandle;
+import io.prestosql.spi.connector.DynamicFilter;
 import io.prestosql.spi.predicate.Domain;
 import io.prestosql.spi.predicate.TupleDomain;
 import io.prestosql.spi.type.StandardTypes;
@@ -62,6 +63,7 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hdfs.BlockMissingException;
 import org.apache.iceberg.FileFormat;
+import org.apache.iceberg.hadoop.HadoopFileIO;
 import org.apache.parquet.column.ColumnDescriptor;
 import org.apache.parquet.hadoop.metadata.BlockMetaData;
 import org.apache.parquet.hadoop.metadata.FileMetaData;
@@ -140,7 +142,7 @@ public class IcebergPageSourceProvider
             ConnectorSplit connectorSplit,
             ConnectorTableHandle connectorTable,
             List<ColumnHandle> columns,
-            TupleDomain<ColumnHandle> dynamicFilter)
+            DynamicFilter dynamicFilter)
     {
         IcebergSplit split = (IcebergSplit) connectorSplit;
         IcebergTableHandle table = (IcebergTableHandle) connectorTable;
@@ -157,18 +159,27 @@ public class IcebergPageSourceProvider
                 .collect(toImmutableList());
 
         HdfsContext hdfsContext = new HdfsContext(session, table.getSchemaName(), table.getTableName());
-        ConnectorPageSource dataPageSource = createDataPageSource(
-                session,
-                hdfsContext,
-                new Path(split.getPath()),
-                split.getStart(),
-                split.getLength(),
-                split.getFileSize(),
-                split.getFileFormat(),
-                regularColumns,
-                table.getUnenforcedPredicate());
 
-        return new IcebergPageSource(icebergColumns, partitionKeys, dataPageSource, session.getTimeZoneKey());
+        switch (table.getTableType()) {
+            case DATA:
+                ConnectorPageSource dataPageSource = createDataPageSource(
+                        session,
+                        hdfsContext,
+                        new Path(split.getPath()),
+                        split.getStart(),
+                        split.getLength(),
+                        split.getFileSize(),
+                        split.getFileFormat(),
+                        regularColumns,
+                        table.getUnenforcedPredicate());
+                return new IcebergPageSource(icebergColumns, partitionKeys, dataPageSource, session.getTimeZoneKey());
+            case FILES:
+                Path path = new Path(split.getPath());
+                HadoopFileIO fileIO = new HadoopFileIO(hdfsEnvironment.getConfiguration(hdfsContext, path));
+                return new FilesPageSource(fileIO, icebergColumns, split.getPath());
+            default:
+                throw new PrestoException(IcebergErrorCode.ICEBERG_UNKNOWN_TABLE_TYPE, "not supported" + table.getTableType());
+        }
     }
 
     private ConnectorPageSource createDataPageSource(