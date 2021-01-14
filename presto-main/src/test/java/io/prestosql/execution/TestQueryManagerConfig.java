Parth-Brahmbhatt/presto

[NETFLIX-BUILD]: Add support for query max data size
(cherry picked from commit 369cb32cfc526cc5d72520438069aa775c970283)
(cherry picked from commit 4851902a8b1eadefa6a05f8ccdc08eb8b429b0e5)

presto-main/src/test/java/com/facebook/presto/execution/TestQueryManagerConfig.java
@@ -15,11 +15,15 @@ package com.facebook.presto.execution;
 
 import com.google.common.collect.ImmutableMap;
 import io.airlift.configuration.testing.ConfigAssertions;
+import io.airlift.units.DataSize;
 import io.airlift.units.Duration;
 import org.testng.annotations.Test;
 
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+
+import static io.airlift.units.DataSize.Unit.GIGABYTE;
+import static io.airlift.units.DataSize.Unit.TERABYTE;
 
 public class TestQueryManagerConfig
 {
@@ -46,7 +50,8 @@ public class TestQueryManagerConfig
                 .setQueryMaxExecutionTime(new Duration(100, TimeUnit.DAYS))
                 .setQueryMaxCpuTime(new Duration(1_000_000_000, TimeUnit.DAYS))
                 .setInitializationRequiredWorkers(1)
-                .setInitializationTimeout(new Duration(5, TimeUnit.MINUTES)));
+                .setInitializationTimeout(new Duration(5, TimeUnit.MINUTES))
+                .setQueryMaxDataSize(new DataSize(100, TERABYTE)));
     }
 
     @Test
@@ -73,6 +78,7 @@ public class TestQueryManagerConfig
                 .put("query.max-cpu-time", "2d")
                 .put("query-manager.initialization-required-workers", "200")
                 .put("query-manager.initialization-timeout", "1m")
+                 .put("query.max-data-size", "10GB")
                 .build();
 
         QueryManagerConfig expected = new QueryManagerConfig()
@@ -95,7 +101,8 @@ public class TestQueryManagerConfig
                 .setQueryMaxExecutionTime(new Duration(3, TimeUnit.HOURS))
                 .setQueryMaxCpuTime(new Duration(2, TimeUnit.DAYS))
                 .setInitializationRequiredWorkers(200)
-                .setInitializationTimeout(new Duration(1, TimeUnit.MINUTES));
+                .setInitializationTimeout(new Duration(1, TimeUnit.MINUTES))
+                 .setQueryMaxDataSize(new DataSize(10, GIGABYTE));
 
         ConfigAssertions.assertFullMapping(properties, expected);
     }

Parth-Brahmbhatt/presto-1

presto-main/src/test/java/io/prestosql/execution/TestQueryManagerConfig.java
@@ -25,6 +25,8 @@ import static io.airlift.configuration.testing.ConfigAssertions.assertFullMappin
 import static io.airlift.configuration.testing.ConfigAssertions.assertRecordedDefaults;
 import static io.airlift.configuration.testing.ConfigAssertions.recordDefaults;
 import static io.airlift.units.DataSize.Unit.KILOBYTE;
+import static io.airlift.units.DataSize.Unit.GIGABYTE;
+import static io.airlift.units.DataSize.Unit.TERABYTE;
 
 public class TestQueryManagerConfig
 {
@@ -53,7 +55,8 @@ public class TestQueryManagerConfig
                 .setQueryMaxCpuTime(new Duration(1_000_000_000, TimeUnit.DAYS))
                 .setQueryMaxScanPhysicalBytes(null)
                 .setRequiredWorkers(1)
-                .setRequiredWorkersMaxWait(new Duration(5, TimeUnit.MINUTES)));
+                .setRequiredWorkersMaxWait(new Duration(5, TimeUnit.MINUTES))
+                .setQueryMaxDataSize(new DataSize(100, TERABYTE)));
     }
 
     @Test
@@ -82,6 +85,7 @@ public class TestQueryManagerConfig
                 .put("query.max-scan-physical-bytes", "1kB")
                 .put("query-manager.required-workers", "333")
                 .put("query-manager.required-workers-max-wait", "33m")
+                .put("query.max-data-size", "10GB")
                 .build();
 
         QueryManagerConfig expected = new QueryManagerConfig()
@@ -106,7 +110,8 @@ public class TestQueryManagerConfig
                 .setQueryMaxCpuTime(new Duration(2, TimeUnit.DAYS))
                 .setQueryMaxScanPhysicalBytes(DataSize.of(1, KILOBYTE))
                 .setRequiredWorkers(333)
-                .setRequiredWorkersMaxWait(new Duration(33, TimeUnit.MINUTES));
+                .setRequiredWorkersMaxWait(new Duration(33, TimeUnit.MINUTES))
+                .setQueryMaxDataSize(new DataSize(10, GIGABYTE));
 
         assertFullMapping(properties, expected);
     }