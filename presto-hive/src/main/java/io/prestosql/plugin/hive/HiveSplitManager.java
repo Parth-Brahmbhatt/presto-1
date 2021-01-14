Parth-Brahmbhatt/presto

[NETFLIX-BUILD]: Add a feature to cache files in HDFS
(cherry picked from commit 0fa361e8b4339b94d803efbeda8562febfb4181a)

presto-hive/src/main/java/com/facebook/presto/hive/HiveSplitManager.java
@@ -90,6 +90,8 @@ public class HiveSplitManager
     private final int splitLoaderConcurrency;
     private final boolean recursiveDfsWalkerEnabled;
     private final CounterStat highMemorySplitSourceCounter;
+    private PrestoHdfsCache prestoHdfsCache;
+    private boolean isHdfsDeployed;
 
     @Inject
     public HiveSplitManager(
@@ -116,6 +118,11 @@ public class HiveSplitManager
                 hiveClientConfig.getMaxInitialSplits(),
                 hiveClientConfig.getSplitLoaderConcurrency(),
                 hiveClientConfig.getRecursiveDirWalkerEnabled());
+        this.prestoHdfsCache = new PrestoHdfsCache();
+        this.isHdfsDeployed = hiveClientConfig.isHdfsCacheEnabled();
+        if (isHdfsDeployed) {
+            prestoHdfsCache.setHdfsCache(hiveClientConfig.getHdfsCacheDefaultFs(), hiveClientConfig.getHdfsCacheReplicationFactor());
+        }
     }
 
     public HiveSplitManager(
@@ -203,7 +210,9 @@ public class HiveSplitManager
                 directoryLister,
                 executor,
                 splitLoaderConcurrency,
-                recursiveDfsWalkerEnabled);
+                recursiveDfsWalkerEnabled,
+                prestoHdfsCache,
+                isHdfsDeployed);
 
         HiveSplitSource splitSource;
         switch (splitSchedulingStrategy) {

presto-hive/src/main/java/com/facebook/presto/hive/util/HiveFileIterator.java
@@ -15,6 +15,7 @@ package com.facebook.presto.hive.util;
 
 import com.facebook.presto.hive.DirectoryLister;
 import com.facebook.presto.hive.NamenodeStats;
+import com.facebook.presto.hive.PrestoHdfsCache;
 import com.facebook.presto.spi.PrestoException;
 import com.google.common.collect.AbstractIterator;
 import io.airlift.stats.TimeStat;
@@ -51,19 +52,25 @@ public class HiveFileIterator
     private final NestedDirectoryPolicy nestedDirectoryPolicy;
 
     private Iterator<LocatedFileStatus> remoteIterator = Collections.emptyIterator();
+    private final PrestoHdfsCache prestoHdfsCache;
+    private final boolean isHdfsDeployed;
 
     public HiveFileIterator(
             Path path,
             FileSystem fileSystem,
             DirectoryLister directoryLister,
             NamenodeStats namenodeStats,
-            NestedDirectoryPolicy nestedDirectoryPolicy)
+            NestedDirectoryPolicy nestedDirectoryPolicy,
+            boolean isHdfsDeployed,
+            PrestoHdfsCache prestoHdfsCache)
     {
         paths.addLast(requireNonNull(path, "path is null"));
         this.fileSystem = requireNonNull(fileSystem, "fileSystem is null");
         this.directoryLister = requireNonNull(directoryLister, "directoryLister is null");
         this.namenodeStats = requireNonNull(namenodeStats, "namenodeStats is null");
         this.nestedDirectoryPolicy = requireNonNull(nestedDirectoryPolicy, "nestedDirectoryPolicy is null");
+        this.isHdfsDeployed = isHdfsDeployed;
+        this.prestoHdfsCache = prestoHdfsCache;
     }
 
     @Override
@@ -91,6 +98,18 @@ public class HiveFileIterator
                     }
                 }
 
+                if (isHdfsDeployed) {
+                    try {
+                        Path s3Path = status.getPath();
+                        Path s3OrHdfsPath = prestoHdfsCache.getHdfsPathOrCopyToHdfs(s3Path);
+                        if (!s3OrHdfsPath.equals(s3Path)) {
+                            status = prestoHdfsCache.getLocatedStatus(s3OrHdfsPath);
+                        }
+                    }
+                    catch (IOException ignored) {
+                        //do Nothing
+                    }
+                }
                 return status;
             }
 

Parth-Brahmbhatt/presto-1

presto-hive/src/main/java/io/prestosql/plugin/hive/HiveSplitManager.java
@@ -105,6 +105,8 @@ public class HiveSplitManager
     private final boolean recursiveDfsWalkerEnabled;
     private final CounterStat highMemorySplitSourceCounter;
     private final TypeManager typeManager;
+    private PrestoHdfsCache prestoHdfsCache;
+    private boolean isHdfsDeployed;
 
     @Inject
     public HiveSplitManager(
@@ -135,6 +137,12 @@ public class HiveSplitManager
                 hiveConfig.getMaxSplitsPerSecond(),
                 hiveConfig.getRecursiveDirWalkerEnabled(),
                 typeManager);
+                hiveConfig.getRecursiveDirWalkerEnabled();
+        this.prestoHdfsCache = new PrestoHdfsCache();
+        this.isHdfsDeployed = hiveConfig.isHdfsCacheEnabled();
+        if (isHdfsDeployed) {
+            prestoHdfsCache.setHdfsCache(hiveConfig.getHdfsCacheDefaultFs(), hiveConfig.getHdfsCacheReplicationFactor());
+        }
     }
 
     public HiveSplitManager(
@@ -239,7 +247,9 @@ public class HiveSplitManager
                 !hiveTable.getPartitionColumns().isEmpty() && isIgnoreAbsentPartitions(session),
                 isOptimizeSymlinkListing(session),
                 metastore.getValidWriteIds(session, hiveTable)
-                        .map(validTxnWriteIdList -> validTxnWriteIdList.getTableValidWriteIdList(table.getDatabaseName() + "." + table.getTableName())));
+                        .map(validTxnWriteIdList -> validTxnWriteIdList.getTableValidWriteIdList(table.getDatabaseName() + "." + table.getTableName())),
+                prestoHdfsCache,
+                isHdfsDeployed);
 
         HiveSplitSource splitSource;
         switch (splitSchedulingStrategy) {

presto-hive/src/main/java/io/prestosql/plugin/hive/util/HiveFileIterator.java
@@ -17,6 +17,8 @@ import com.google.common.collect.AbstractIterator;
 import io.airlift.stats.TimeStat;
 import io.prestosql.plugin.hive.DirectoryLister;
 import io.prestosql.plugin.hive.NamenodeStats;
+import io.prestosql.plugin.hive.PrestoHdfsCache;
+import io.prestosql.plugin.hive.PrestoHdfsCacheStats;
 import io.prestosql.plugin.hive.metastore.Table;
 import io.prestosql.spi.PrestoException;
 import org.apache.hadoop.fs.FileSystem;
@@ -54,6 +56,8 @@ public class HiveFileIterator
     private final boolean ignoreAbsentPartitions;
 
     private Iterator<LocatedFileStatus> remoteIterator = emptyIterator();
+    private final PrestoHdfsCache prestoHdfsCache;
+    private final boolean isHdfsDeployed;
 
     public HiveFileIterator(
             Table table,
@@ -62,7 +66,9 @@ public class HiveFileIterator
             DirectoryLister directoryLister,
             NamenodeStats namenodeStats,
             NestedDirectoryPolicy nestedDirectoryPolicy,
-            boolean ignoreAbsentPartitions)
+            boolean ignoreAbsentPartitions,
+            boolean isHdfsDeployed,
+            PrestoHdfsCache prestoHdfsCache)
     {
         paths.addLast(requireNonNull(path, "path is null"));
         this.table = requireNonNull(table, "table is null");
@@ -71,6 +77,8 @@ public class HiveFileIterator
         this.namenodeStats = requireNonNull(namenodeStats, "namenodeStats is null");
         this.nestedDirectoryPolicy = requireNonNull(nestedDirectoryPolicy, "nestedDirectoryPolicy is null");
         this.ignoreAbsentPartitions = ignoreAbsentPartitions;
+        this.isHdfsDeployed = isHdfsDeployed;
+        this.prestoHdfsCache = prestoHdfsCache;
     }
 
     @Override
@@ -98,6 +106,18 @@ public class HiveFileIterator
                     }
                 }
 
+                if (isHdfsDeployed) {
+                    try {
+                        Path s3Path = status.getPath();
+                        Path s3OrHdfsPath = prestoHdfsCache.getHdfsPathOrCopyToHdfs(s3Path);
+                        if (!s3OrHdfsPath.equals(s3Path)) {
+                            status = prestoHdfsCache.getLocatedStatus(s3OrHdfsPath);
+                        }
+                    }
+                    catch (IOException ignored) {
+                        //do Nothing
+                    }
+                }
                 return status;
             }
 